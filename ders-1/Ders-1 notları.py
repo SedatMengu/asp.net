be88bdd254630d74bd3bb396a0e9ec511ec331a7
sedat mengu <sedatmengu@gmail.com>
2023-07-11 23:57:34 +0300

    Create Ders-1 notları.py

---
+++ ders-1/Ders-1 notları.py
@@ -0,0 +1,82 @@
+# ilk olarak python.org adresine giderek python kurulumunu yapıyoruz.
+
+# komut satırına pip freeze yazarak kurulu olan paketler karşımıza çıkıyor.
+
+# asgiref==3.6.0
+# certifi==2023.5.7
+# charset-normalizer==3.1.0
+# distlib==0.3.6
+# Django==3.2.19
+# filelock==3.12.0
+# idna==3.4
+# importlib-metadata==6.6.0
+# platformdirs==3.5.1
+# pytz==2023.3
+# requests==2.31.0
+# sqlparse==0.4.4
+# typing_extensions==4.5.0
+# urllib3==2.0.2
+# virtualenv==20.23.0
+# virtualenvwrapper-win==1.2.7
+# zipp==3.15.0
+
+
+# django nun son sürümünü kuruyoruz.
+# kurulu django sürümünü silmek istersek komut satırına :
+#  - "pip uninstall django" yazabiliriz.
+
+
+# - django 3.2.9 versiyonunu yüklemek için cmd komut satırına :
+
+# pip install django==3.2.9 yazıyoruz.
+
+# yüklendikten sonra kontrol için pip freeze yazıyoruz ve
+
+# asgiref==3.6.0
+# certifi==2023.5.7
+# charset-normalizer==3.1.0
+# distlib==0.3.6
+# Django==3.2.9
+# filelock==3.12.0
+# idna==3.4
+# importlib-metadata==6.6.0
+# platformdirs==3.5.1
+# pytz==2023.3
+# requests==2.31.0
+# sqlparse==0.4.4
+# typing_extensions==4.5.0
+# urllib3==2.0.2
+# virtualenv==20.23.0
+# virtualenvwrapper-win==1.2.7
+# zipp==3.15.0
+
+# Django 3.2.9 kurulu olan paketler içerisinde yer aldı.
+
+# django-admin yazarak django yönetim komutlarına ulaşabiliriz.
+
+# check                 : Django projesinde hataları kontrol etmek için kullanılır.
+# compilemessages       : Çeviri dosyalarını derlemek için kullanılır.
+# createcachetable      : Önbellek tablosunu veritabanında oluşturmak için kullanılır.
+# dbshell               : Veritabanı yönetim konsolunu açmak için kullanılır.
+# diffsettings          : Ayar dosyaları arasındaki farkları göstermek için kullanılır.
+# dumpdata              : Veritabanındaki verileri dışa aktarmak için kullanılır.
+# flush                 : Veritabanındaki tüm verileri silmek için kullanılır.
+# inspectdb             : Varolan bir veritabanından Django model dosyalarını oluşturmak için kullanılır.
+# loaddata              : Veritabanına dışa aktarılmış verileri içe aktarmak için kullanılır.
+# makemessages          : Çeviri dosyalarını oluşturmak veya güncellemek için kullanılır.
+# makemigrations        : Veritabanı şemalarında değişiklik yapan migration dosyalarını oluşturmak için kullanılır.
+# migrate               : Migration dosyalarını veritabanına uygulamak için kullanılır.
+# runserver             : Django geliştirme sunucusunu başlatmak için kullanılır.
+# sendtestemail         : Test e-postası göndermek için kullanılır.
+# shell                 : Django projesi için etkileşimli Python kabuğunu başlatmak için kullanılır.
+# showmigrations        : Mevcut migration durumunu göstermek için kullanılır.
+# sqlflush              : Veritabanını sıfırlamak için SQL ifadelerini görüntüler.
+# sqlmigrate            : Migration dosyasını SQL ifadeleri olarak görüntüler.
+# sqlsequencereset      : Otomatik artan sıraları sıfırlamak için SQL ifadelerini görüntüler.
+# squashmigrations      : Migration dosyalarını birleştirmek için kullanılır.
+# startapp              : Yeni bir Django uygulama klasörü oluşturmak için kullanılır.
+# startproject          : Yeni bir Django projesi oluşturmak için kullanılır.
+# test                  : Django projesindeki testleri çalıştırmak için kullanılır.
+# testserver            : Geliştirme sunucusunda test veritabanı ile Django projesini çalıştırmak için kullanılır.
+
+
