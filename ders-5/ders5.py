d93a568d7465b9c5dbeb23ea189e4f0be770f870
Sedat MENGÜ <sedatmengu@gmail.com>
2023-07-20 23:56:21 +0300

    .

---
+++ ders-5/ders5.py
@@ -0,0 +1,12 @@
+# bootstrap şablonları ekleyeceğiz.
+
+# indirdiğimiz dosyadan sadece source klasörünü almıyoruz , onun dışındak bütün dosyaları projemiz içerisindeki wwwroot içerisine ekliyoruz.
+# wwwroot kısmı içerisindeki lib klasörünü harici tutarak geri kanaların tamamını siliyoruz.
+
+#  readme dosyasını da sildik. peyderpey html dosyalarını sileceğiz.
+
+# wwwroot içerisindeki css dosyası içerisine birtane site.css adında bir klasör oluşturuyoruz.
+
+# wwwroot içerisindeki js dosyası içerisine birtane js.css adında bir klasör oluşturuyoruz.
+
+# wwwroot içerisine upload adında bir klasör oluşturuyoruz , altına images ve files adında 2 klasör daha oluşturuyoruz.
