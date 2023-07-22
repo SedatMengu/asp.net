a274e707be9917379c5cbe03e2130e3f66341908
Sedat MENGÜ <sedatmengu@gmail.com>
2023-07-22 17:43:14 +0300

    .

---
+++ ders-6/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using E_Ticaret.Models;
+
+namespace E_Ticaret.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [Route("/shop")]
+    public IActionResult Shop()
+    {
+        return View();
+    }
+    [Route("/blog")]
+    public IActionResult Blog()
+    {
+        return View();
+    }
+    [Route("/contact")]
+    public IActionResult Contact()
+    {
+        return View();
+    }
+    [Route("/login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    [Route("/register")]
+    public IActionResult Register()
+    {
+        return View();
+    }
+    [Route("/about")]
+    public IActionResult About()
+    {
+        return View();
+    }
+    [Route("/checkout")]
+    public IActionResult Checkout()
+    {
+        return View();
+    }
+    [Route("/faq")]
+    public IActionResult FAQ()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
